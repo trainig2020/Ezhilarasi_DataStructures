3f8cdaf5fb0aef2ad9eaa3743aefd46c91d8a5f9
trainig2020 <68064760+trainig2020@users.noreply.github.com>
2020-09-22 19:53:59 +0530

    Add files via upload

---
+++ SinglyLinkedListOperations/src/com/ezhil/LinkedList.java
@@ -0,0 +1,155 @@
+package com.ezhil;
+
+public class LinkedList {
+	
+	protected Node start;
+    protected Node end ;
+    public int size ;
+ 
+   
+    public LinkedList()
+    {
+        start = null;
+        end = null;
+        size = 0;
+    }
+    /*  Function to check if list is empty  */
+    public boolean isEmpty()
+    {
+        return start == null;
+    }
+    /*  Function to get size of list  */
+    public int getSize()
+    {
+        return size;
+    }    
+    /*  Function to insert an element at begining  */
+    public void insertAtStart(int val)
+    {
+        Node nptr = new Node(val, null);    
+        size++ ;    
+        if(start == null) 
+        {
+            start = nptr;
+            end = start;
+        }
+        else 
+        {
+            nptr.setNext(start);
+            start = nptr;
+        }
+    }
+    /*  Function to insert an element at end  */
+    public void insertAtEnd(int val)
+    {
+        Node nptr = new Node(val,null);    
+        size++ ;    
+        if(start == null) 
+        {
+            start = nptr;
+            end = start;
+        }
+        else 
+        {
+            end.setNext(nptr);
+            end = nptr;
+        }
+    }
+    /*  Function to insert an element at position  */
+    public void insertAtPos(int val , int pos)
+    {
+        Node nptr = new Node(val, null);                
+        Node ptr = start;
+        pos = pos - 1 ;
+        for (int i = 1; i < size; i++) 
+        {
+            if (i == pos) 
+            {
+                Node tmp = ptr.getNext() ;
+                ptr.setNext(nptr);
+                nptr.setNext(tmp);
+                break;
+            }
+            ptr = ptr.getNext();
+        }
+        size++ ;
+    }
+    /*  Function to delete an element at position  */
+    public void deleteAtPos(int pos)
+    {        
+        if (pos == 1) 
+        {
+            start = start.getNext();
+            size--; 
+            return ;
+        }
+        if (pos == size) 
+        {
+            Node s = start;
+            Node t = start;
+            while (s != end)
+            {
+                t = s;
+                s = s.getNext();
+            }
+            end = t;
+            end.setNext(null);
+            size --;
+            return;
+        }
+        Node ptr = start;
+        pos = pos - 1 ;
+        for (int i = 1; i < size - 1; i++) 
+        {
+            if (i == pos) 
+            {
+                Node tmp = ptr.getNext();
+                tmp = tmp.getNext();
+                ptr.setNext(tmp);
+                break;
+            }
+            ptr = ptr.getNext();
+        }
+        size-- ;
+    }  
+    
+    public void reverse() 
+    { 
+        Node prev = null; 
+        Node current = start; 
+        Node next = null; 
+        while (current != null) { 
+            next = current.next; 
+            current.next = prev; 
+            prev = current; 
+            current = next; 
+        } 
+        start=prev;
+         
+    } 
+    /*  Function to display elements  */
+    public void display()
+    {
+        System.out.print("\nSingly Linked List = ");
+        if (size == 0) 
+        {
+            System.out.print("empty\n");
+            return;
+        }    
+        if (start.getNext() == null) 
+        {
+            System.out.println(start.getData() );
+            return;
+        }
+        Node ptr = start;
+        System.out.print(start.getData()+ " -> ");
+        ptr = start.getNext();
+        while (ptr.getNext() != null)
+        {
+            System.out.print(ptr.getData()+ " -> ");
+            ptr = ptr.getNext();
+        }
+        System.out.print(ptr.getData()+ "\n");
+    }
+}
+
