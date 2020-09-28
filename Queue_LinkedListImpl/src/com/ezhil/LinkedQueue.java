814abde619f69c79734f60d49c4c1108bdd4f7a4
trainig2020 <68064760+trainig2020@users.noreply.github.com>
2020-09-28 08:44:09 +0530

    Add files via upload

---
+++ Queue_LinkedListImpl/src/com/ezhil/LinkedQueue.java
@@ -0,0 +1,88 @@
+package com.ezhil;
+
+import java.util.NoSuchElementException;
+
+class LinkedQueue
+{
+    protected Node front, rear;
+    public int size;
+ 
+    /* Constructor */
+    public LinkedQueue()
+    {
+        front = null;
+        rear = null;
+        size = 0;
+    }    
+    /*  Function to check if queue is empty */
+    public boolean isEmpty()
+    {
+        return front == null;
+    }    
+    /*  Function to get the size of the queue */
+    public int getSize()
+    {
+        return size;
+    }    
+    /*  Function to insert an element to the queue */
+    public void insert(int data)
+    {
+        Node nptr = new Node(data, null);
+        if (rear == null)
+        {
+            front = nptr;
+            rear = nptr;
+        }
+        else
+        {
+            rear.setNext(nptr);
+            rear = rear.getNext();
+        }
+        size++ ;
+    }    
+    /*  Function to remove front element from the queue */
+    public int remove()
+    {
+        if (isEmpty() )
+            throw new NoSuchElementException("Underflow Exception");
+        Node ptr = front;
+        front = ptr.getNext();        
+        if (front == null)
+            rear = null;
+        size-- ;        
+        return ptr.getData();
+    }    
+    /*  Function to check the front element of the queue */
+    public int peek()
+    {
+        if (isEmpty() )
+            throw new NoSuchElementException("Underflow Exception");
+        return front.getData();
+    }    
+    /*  Function to display the status of the queue */
+    public void display()
+    {
+        System.out.print("\nQueue = ");
+        if (size == 0)
+        {
+            System.out.print("Empty\n");
+            return ;
+        }
+        
+        
+        if (front.getNext() == null) 
+        {
+            System.out.println(front.getData() );
+            return;
+        }
+        Node ptr = front;
+        System.out.print(front.getData()+ " -> ");
+        ptr = front.getNext();
+        while (ptr.getNext() != null)
+        {
+            System.out.print(ptr.getData()+ " -> ");
+            ptr = ptr.getNext();
+        }
+        System.out.print(ptr.getData()+ "\n");     
+    }
+}
